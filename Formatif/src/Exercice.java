70be3ec3cffda7e5b9f17529b9b7da00e51bf9aa
Janvier Zagabe <jzagabe@tfs.ca>
2020-12-11 13:53:17 -0500

    11 décembre

---
+++ Formatif/src/Exercice.java
@@ -0,0 +1,16 @@
+/*
+* @Author : jzagabe
+* @Date : Dec. 11, 2020
+* @Description : 
+*/
+
+public class Exercice
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
